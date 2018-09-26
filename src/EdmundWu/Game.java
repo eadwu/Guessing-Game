54f0f2ca5aceb0142c87540c06d3bea93498ee3d
Edmund Wu <fangkazuto@gmail.com>
2018-09-25 21:11:51 -0400

    Refactor imported code
    Abstract some parts into EdmundWu.Game

---
+++ src/EdmundWu/Game.java
@@ -0,0 +1,57 @@
+package EdmundWu;
+
+import java.util.Scanner;
+
+public class Game {
+    String name;
+    int gameType;
+    int max;
+    int number;
+
+    private void fetchName (Scanner stdin) {
+        System.out.println("What is your name?");
+        this.name = stdin.nextLine();
+        System.out.printf("Hello there, %s.\n", this.name);
+    }
+
+    private void fetchGameType (Scanner stdin) {
+        System.out.println("What type of game do you want to play, 0 - Player guesses, 1 - Computer guesses?");
+        int gameType = stdin.nextInt();
+
+        if (gameType < 0 || gameType > 1) {
+            System.out.println("Incorrect value for gameType, exiting...");
+            System.exit(1);
+        }
+        this.gameType = gameType;
+    }
+
+    private void fetchDifficulty (Scanner stdin) {
+        System.out.println("What difficulty do you want to choose, 1-4, transforms range from [1, 10^n].");
+        int difficulty = stdin.nextInt();
+
+        if (difficulty < 1 || difficulty > 4) {
+            System.out.println("Incorrect value for difficulty, exiting...");
+            System.exit(1);
+        }
+        this.max = (int) Math.pow(10, difficulty);
+    }
+
+    private void getNumber (Scanner stdin) {
+        if (gameType == 1)
+            System.out.printf("Choose a number from [1, %d].\n", (int) max);
+        int number = gameType == 1 ? stdin.nextInt() : (int) (Math.random() * max + 1f);
+
+        if (number < 1 || number > max) {
+            System.out.println("Invalid value for max, exiting...");
+            System.exit(1);
+        }
+        this.number = number;
+    }
+
+    public Game (Scanner stdin) {
+        this.fetchName(stdin);
+        this.fetchGameType(stdin);
+        this.fetchDifficulty(stdin);
+        this.getNumber(stdin);
+    }
+}
---
+++ src/EdmundWu/Main.java
@@ -0,0 +1,59 @@
+package EdmundWu;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int guesses = 0;
+
+        Game mainframe = new EdmundWu.Game(stdin);
+
+        int max = mainframe.max;
+        String name = mainframe.name;
+        int number = mainframe.number;
+        int gameType = mainframe.gameType;
+
+        int currentGuess = (int) (max / 2f);
+        int lastGuess = max;
+
+        while (true) {
+            guesses++;
+            if (gameType == 0) {
+                System.out.printf("Guess a number, %s.\n", name);
+
+                int guess = stdin.nextInt();
+                if (guess == number) {
+                    System.out.printf("You have chosen the correct number, %s.\n", name);
+                    System.out.printf("It took you %d guesses to find the answer.", guesses);
+                    break;
+                }
+            } else {
+                if (currentGuess == number) {
+                    System.out.printf("The AI has found the correct number, %s.\n", name);
+                    System.out.printf("It took it %d guesses to find the number %d.\n", guesses, number);
+                    break;
+                }
+
+                System.out.printf("The guessed number is %d, is the number higher than the guess (1) or lower than it (2)?\n",
+                        currentGuess);
+                int highOrLow = stdin.nextInt();
+
+                if (highOrLow < 1 || highOrLow > 2 ||
+                        (highOrLow == 1 && currentGuess > number) ||
+                        (highOrLow == 2 && currentGuess < number)) {
+                    System.out.println("Invalid value for highOrLow, exiting...");
+                    System.exit(1);
+                }
+
+                float difference = Math.abs(currentGuess - lastGuess) / 2f;
+                lastGuess = currentGuess;
+                currentGuess = highOrLow == 1
+                        ? Math.round(currentGuess + difference)
+                        : Math.round(currentGuess - difference);
+            }
+        }
+        stdin.close();
+    }
+}
